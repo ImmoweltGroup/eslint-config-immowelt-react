b13658b6cc150bdb03b92ac428ac06665418fcae
Kevin Ruff <kevin.ruff@immonet.de>
2018-10-12 16:33:47 +0200

    FEATURE: Update file extension rule.

--- rules.js
+++ rules.js
@@ -1,8 +1,9 @@
 module.exports = {
-  'jsx-a11y/label-has-for': 'off',
-  'jsx-a11y/no-static-element-interactions': 'off',
+  'react/jsx-filename-extension': ['warn', {extensions: ['.js', '.jsx']}],
   'react/jsx-boolean-value': 'error',
   'react/prop-types': 'error',
   'react/require-default-props': 'error',
-  'react/forbid-component-props': 'error'
+  'react/forbid-component-props': 'error',
+  'jsx-a11y/label-has-for': 'off',
+  'jsx-a11y/no-static-element-interactions': 'off'
 };
